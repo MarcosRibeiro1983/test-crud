2e84801a78560a183560405959849f0a7cbc82ed
Marcos <marcos@softcontext.com>
2025-02-05 20:46:56 -0300

    docker

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '../service/user.service';
 
 describe('UserListComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
 
+  class MockUserService {
+    name = 'John Doe';
+    age = 20;
+    desc = 'testDesc'
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserComponent]
+      imports: [UserComponent, FormsModule, ReactiveFormsModule],
+      providers: [ UserComponent, { provide: UserService, useClass: MockUserService } ]
     })
     .compileComponents();
 
@@ -19,6 +28,10 @@ describe('UserListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create', () => {
+    expect(component.create()).toBeTruthy();
   });
   
 });
